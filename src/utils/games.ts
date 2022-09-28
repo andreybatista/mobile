3475ecaa9cc4fee83ef7cad69092c2e50f8555f1
andrey batista <andreybatista131@gmail.com>
2022-09-28 15:44:47 -0300

    Nlw

---
+++ src/utils/games.ts
@@ -0,0 +1,38 @@
+export const GAMES = [
+    {
+        id: '1',
+        name: 'League of Leagends',
+        ads: '4',
+        cover: require('../assets/games/game-1.png')
+    },
+    {
+        id: '2',
+        name: 'Dota 2',
+        ads: '4',
+        cover: require('../assets/games/game-1.png')
+    },
+    {
+        id: '3',
+        name: 'CS-GO',
+        ads: '4',
+        cover: require('../assets/games/game-1.png')
+    },
+    {
+        id: '4',
+        name: 'Apex',
+        ads: '4',
+        cover: require('../assets/games/game-1.png')
+    },
+    {
+        id: '5',
+        name: 'League of Leagends2',
+        ads: '4',
+        cover: require('../assets/games/game-1.png')
+    },
+    {
+        id: '6',
+        name: 'League of Leagends3',
+        ads: '4',
+        cover: require('../assets/games/game-1.png')
+    }
+]
